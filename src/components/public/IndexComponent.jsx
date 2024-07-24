b746e3cac9c5242180da49a2534ae262e329c95b
Dustin Parker <dparker@hopetemecula.org>
2024-07-24 13:28:33 -0700

    From Work

--- src/components/public/IndexComponent.jsx
+++ src/components/public/IndexComponent.jsx
@@ -1,23 +1,50 @@
-import { Image, Button, Card} from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // Import Link component
+import {
+  Image,
+  Container,
+  Row,
+  Col,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  ListGroup,
+} from "react-bootstrap";
+
+import { FiBookOpen } from "react-icons/fi";
 
 export default function IndexComponent() {
   return (
     <>
-      <div>
-        <Image src="https://hopetemecula.org/wp-content/uploads/2015/03/newlogo-e1597113852251.jpg" />
-      </div>
-      <h1>Hope Training App</h1>
-      <Card className='bg-light'>
-        <Card.Header>
-            Lighting Trainer
-        </Card.Header>
-        <Card.Body>
-            <Link to="/lighting">
-             <Button>Go to lighting</Button>
-            </Link>
-        </Card.Body>
-      </Card>
+      <Container fluid style={{ height: "80vh" }}>
+        <Row>
+          <Col>
+            <Image src="https://hopetemecula.org/wp-content/uploads/2015/03/newlogo-e1597113852251.jpg" />
+
+            <h1>Hope Training App</h1>
+            <h3 style={{ color: "rgba(0,0,0,0)" }}>
+              <FiBookOpen color="rgba(0,0,0,.80)" size={30} />
+              <span>- - - - - - - - - - - -</span>
+              <FiBookOpen color="rgba(0,0,0,.80)" size={30} />
+            </h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col>
+            <br />
+            <Dropdown as={ListGroup}>
+              <Dropdown.Toggle variant="secondary" id="dropdown">
+                Select a training
+              </Dropdown.Toggle>
+
+              <DropdownMenu variant="dark">
+                <DropdownItem href="/lighting">Lighting Training</DropdownItem>
+                <DropdownItem disabled>More to Come ...</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- src/components/public/Lighting.jsx
+++ src/components/public/Lighting.jsx
@@ -1,62 +1,66 @@
-import { Card, Row, Col } from 'react-bootstrap';
-import { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import { Card, Container, Col, Row } from "react-bootstrap";
+import { useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 function LightingComponent() {
-    const [pageNumber, setPageNumber] = useState(1);
-    const maxPages = 5;
-    function advancePage() {
-        if (pageNumber < maxPages && pageNumber > 0) {
-            setPageNumber(pageNumber + 1)
-        }
-        return;
+  const [pageNumber, setPageNumber] = useState(1);
+  const maxPages = 5;
+  function advancePage() {
+    if (pageNumber < maxPages && pageNumber > 0) {
+      setPageNumber(pageNumber + 1);
     }
+    return;
+  }
 
-    function goBack(){
-        if (pageNumber <= maxPages && pageNumber > 1) {
-            setPageNumber(pageNumber - 1)
-        } 
-        return;
+  function goBack() {
+    if (pageNumber <= maxPages && pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
     }
+    return;
+  }
 
-    let ItemRender = null;
+  let ItemRender = null;
 
-    switch (pageNumber) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            ItemRender = <Card.Text className=''>Page {pageNumber}</Card.Text>;
-            break;
+  switch (pageNumber) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      ItemRender = <Card.Text>Page {pageNumber}</Card.Text>;
+      break;
 
-        default: ItemRender = <Card.Text>Page Unknown</Card.Text>
-    }
-    return (
-
-        <>
-            <Navbar>
-                <NavbarBrand>
-
-                    <img
-                        src="https://hopetemecula.org/wp-content/uploads/2015/03/newlogo-e1597113852251.jpg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                        alt="HLC logo" />{" "}
-                    HLC
-
-                </NavbarBrand>
-                <Nav activeKey={"Lighting"}>
-                    <NavLink key={"/"} href='/'>Home</NavLink>
-                    <NavLink key={"lighting"} href='/lighting'>Lighting</NavLink>
-                </Nav>
-            </Navbar>
-            <Container>
-            </Container>
-        </>
-
-    );
+    default:
+      ItemRender = <Card.Text>Page Unknown</Card.Text>;
+  }
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>
+                <h3>Lighting Training</h3>
+              </Card.Header>
+              <Card.Body>
+                <Card.Img
+                  variant="top"
+                  src=""
+                ></Card.Img>
+                {ItemRender}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={2}></Col>
+          <Col lg={8}></Col>
+          <Col lg={2}></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default LightingComponent;

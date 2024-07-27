a3cfb4f8a546430df58d6dbb36bbca6248d2010a
Dustin Parker <dparker@hopetemecula.org>
2024-07-27 16:40:50 -0700

    added help to nav

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,22 +1,39 @@
-import { Image, Navbar, Nav, NavItem, NavLink} from 'react-bootstrap';
-import { useState } from 'react';
+import { Image, Navbar, Nav, NavItem, NavLink } from "react-bootstrap";
+import { BiHelpCircle } from "react-icons/bi";
+import { useState } from "react";
 
 export default function NavComponent() {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar variant='light' bg='light' fixed='top' expand="md">
+    <Navbar variant="light" bg="light" fixed="top" expand="md">
       <Navbar.Brand>
-        <Image src="src/assets/images/Hope-logo.png" alt="Your Logo" width={50}/>
+        <Image
+          src="src/assets/images/Hope-logo.png"
+          alt="Your Logo"
+          width={50}
+        />
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        onClick={() => setExpanded(!expanded)}
+      />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className='ms-auto'>
+        <Nav className="ms-auto" defaultActiveKey={"home"}>
           <NavItem>
-            <NavLink href="/">Home</NavLink>
+            <NavLink key={"home"} href="/">
+              Home
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/Lighting">Lighting</NavLink>
+            <NavLink key={"lighting"} href="/Lighting">
+              Lighting
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink key={"lighting"} href="/Lighting">
+              Help <BiHelpCircle />
+            </NavLink>
           </NavItem>
         </Nav>
       </Navbar.Collapse>

c4401fc60e5ee1b84c840f61e8384e903cfa53c4
Dustin Parker <dparker@hopetemecula.org>
2024-07-27 17:13:11 -0700

    update navbar for search and help

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,9 +1,20 @@
-import { Image, Navbar, Nav, NavItem, NavLink } from "react-bootstrap";
+import {
+  Image,
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  Tooltip,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 import { BiHelpCircle } from "react-icons/bi";
 import { useState } from "react";
 
 export default function NavComponent() {
   const [expanded, setExpanded] = useState(false);
+  const [isActive, setIsActive] = useState("home");
 
   return (
     <Navbar variant="light" bg="light" fixed="top" expand="md">
@@ -21,19 +32,31 @@ export default function NavComponent() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto" defaultActiveKey={"home"}>
           <NavItem>
-            <NavLink key={"home"} href="/">
+            <NavLink eventKey={"home"} href="/">
               Home
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink key={"lighting"} href="/Lighting">
+            <NavLink eventKey={"lighting"} href="/lighting">
               Lighting
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink key={"lighting"} href="/Lighting">
-              Help <BiHelpCircle />
+            <NavLink eventKey={"help"} href="/help">
+              <BiHelpCircle size={20} />
             </NavLink>
+          </NavItem>
+          <NavItem>
+            <Form className="d-flex align-items-center">
+              <FormControl type="input" placeholder="Search" id="search-bar" />
+              <Button
+                variant="dark"
+                className="ms-2"
+                onClick={() => console.log("Search Term - ")}
+              >
+                Go
+              </Button>
+            </Form>
           </NavItem>
         </Nav>
       </Navbar.Collapse>

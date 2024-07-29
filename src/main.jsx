cf6102c8ccac2d0402f4beb56853f54f16a99827
Dustin Parker <pagesbydustin@gmail.com>
2024-07-29 08:07:57 +0000

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 
+import '/src/assets/css/bootstrap_lumen-theme.min.css';
+import '/src/assets/css/index.css';
+import '/src/assets/css/App.css';
+
 
 
 
